4c45c81b8e19cdf2242697d19dafe1cb89d34e0c
Showaikh <alialshowaikh96@gmail.com>
2023-11-12 11:46:42 +0300

    phonebook

---
+++ README.md
@@ -0,0 +1,18 @@
+# phonebook.py
+light python script to generate a phonebook of all phone numbers belonging to a specific zone/sim
+
+# Usage:
+
+python phonebook.py
+------------------------------------------------------
+filename: name the file that will contain the phonebook
+
+Ex: viva.txt
+---------------------------------------------------------------------------
+extension: the extension number associated with the country or SIM provider
+
+Ex: 17
+--------------------------------------------------------------------------------------------
+digits: "ENTERED AS 0s" the amount of digits involved in the number (without the extesnsion)
+
+Ex: 000000
---
+++ phonebook.py
@@ -0,0 +1,25 @@
+
+print("""
+ _                   _ _   ___            _ 
+| | _____  ___ _ __ / | |_( _ )  ___  ___| |
+| |/ / _ \/ _ \ '_ \| | __/ _ \ / _ \/ _ \ |
+|   <  __/  __/ |_) | | || (_) |  __/  __/ |
+|_|\_\___|\___| .__/|_|\__\___/ \___|\___|_|
+              |_|                           
+""")
+
+class phone:
+        file = input("filename:")
+        file = open(file, "w+")
+        ext = input("extension:")
+        digits = input("digits:")
+        added = "1" + digits
+        converted = int(added)
+        for i in range(converted):
+            i = f'{i:0{len(digits)}}\n'
+            i = str (i)
+            file.write (ext + i)
+
+
+
+
